daae7c2e288efff24e9cea3f7f8a85a97e5b644e
Irek <general157@mail.ru>
2022-12-13 11:03:06 +0300

    reverseWords is added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Solution solution = new Solution();
-        solution.merge(new int[]{1,2,3,0,0,0},3, new int[]{2,5,6}, 3);
+        Solution sln = new Solution();
+        System.out.println(sln.reverseWords("the sky is blue"));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -38,6 +38,30 @@ public class Solution {
             }
         }
     }
+
+    public String reverseWords(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        int end = s.length() - 1;
+        while (end >= 0) {
+            if (s.charAt(end) == ' ') {
+                end--;
+                continue;
+            }
+            int start = end - 1;
+            while (start >= 0 && s.charAt(start) != ' ') {
+                start--;
+            }
+            sb.append(' ');
+            sb.append(s.substring(start + 1, end + 1));
+            end = start - 1;
+        }
+        if (sb.length() > 0) {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+
 }
 
 
